f3ca96dcf0ec1c235aa678cb996c419a52861661
adsasmita <andridsasmita@gmail.com>
2017-03-16 11:32:09 +0700

    Edit scan

--- scan.py
+++ scan.py
@@ -5,15 +5,17 @@ import rect
 # add image here.
 image = cv2.imread('test_pic.jpg')
 
-# resize image so it can be processed
-# choose optimal dimensions such that important content is not lost
+# resize image
+# choose optimal dimensions
 image = cv2.resize(image, (1500, 880))
 
-# creating copy of original image
+# create copy of original image
 orig = image.copy()
 
 # convert to grayscale and blur to smooth
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+# gaussian blur to smoothen texture
 blurred = cv2.GaussianBlur(gray, (5, 5), 0)
 #blurred = cv2.medianBlur(gray, 5)
 
@@ -21,8 +23,9 @@ blurred = cv2.GaussianBlur(gray, (5, 5), 0)
 edged = cv2.Canny(blurred, 0, 50)
 orig_edged = edged.copy()
 
-# find the contours in the edged image, keeping only the
-# largest ones, and initialize the screen contour
+# find the contours in the edged image 
+# keep only the largest ones, and 
+# initialize screen contour
 (contours, _) = cv2.findContours(edged, cv2.RETR_LIST, cv2.CHAIN_APPROX_NONE)
 contours = sorted(contours, key=cv2.contourArea, reverse=True)
 
@@ -39,7 +42,7 @@ for c in contours:
         break
 
 
-# mapping target points to 800x800 quadrilateral
+# map target points to 800x800 quadrilateral
 approx = rect.rectify(target)
 pts2 = np.float32([[0,0],[800,0],[800,800],[0,800]])
 
@@ -50,7 +53,7 @@ cv2.drawContours(image, [target], -1, (0, 255, 0), 2)
 dst = cv2.cvtColor(dst, cv2.COLOR_BGR2GRAY)
 
 
-# using thresholding on warped image to get scanned effect (If Required)
+# use thresholding on warped image to get scanned effect (If Required)
 ret,th1 = cv2.threshold(dst,127,255,cv2.THRESH_BINARY)
 th2 = cv2.adaptiveThreshold(dst,255,cv2.ADAPTIVE_THRESH_MEAN_C,\
             cv2.THRESH_BINARY,11,2)
@@ -58,7 +61,7 @@ th3 = cv2.adaptiveThreshold(dst,255,cv2.ADAPTIVE_THRESH_GAUSSIAN_C,\
             cv2.THRESH_BINARY,11,2)
 ret2,th4 = cv2.threshold(dst,0,255,cv2.THRESH_BINARY+cv2.THRESH_OTSU)
 
-
+# show results
 cv2.imshow("Original.jpg", orig)
 cv2.imshow("Original Gray.jpg", gray)
 cv2.imshow("Original Blurred.jpg", blurred)
